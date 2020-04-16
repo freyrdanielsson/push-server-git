05a7bb458fd365439616324e370c6be6c30dce31
freyrdanielsson <freyrdanielsson@gmail.com>
2020-04-16 11:28:52 +0200

    log options

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ var crypto = require('crypto');
 const cors = require('./cors');
 
 const {
-    PORT: port = 5000,
-    HOST: host = '127.0.0.1',
+  PORT: port = 5000,
+  HOST: host = '127.0.0.1',
 } = process.env;
 
 // Take from env irl
 const vapidKeys = {
-    publicKey: 'BIEFQJ0uWcoNDGFm65HFEKjTsSgGgwaaGi-Hz4jASPLF4wY0Py8fn_gwJChrKsBXyS7WNC-INuNhvEB3456_gnY',
-    privateKey: '_U9F8BfDH0DfFBv2MJ_qDu0Z1ye4wjGCH2oJhj9yBJ4'
+  publicKey: 'BIEFQJ0uWcoNDGFm65HFEKjTsSgGgwaaGi-Hz4jASPLF4wY0Py8fn_gwJChrKsBXyS7WNC-INuNhvEB3456_gnY',
+  privateKey: '_U9F8BfDH0DfFBv2MJ_qDu0Z1ye4wjGCH2oJhj9yBJ4'
 };
 
 webpush.setVapidDetails('mailto:freyrdanielsson@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey);
@@ -26,45 +26,49 @@ app.use(express.json());
 app.use(cors);
 
 function createHash(input) {
-    const md5sum = crypto.createHash('md5');
-    md5sum.update(Buffer.from(input));
-    return md5sum.digest('hex');
+  const md5sum = crypto.createHash('md5');
+  md5sum.update(Buffer.from(input));
+  return md5sum.digest('hex');
 }
 
 // TODO: Store persistently
 const subscriptions = {};
 function handlePushNotificationSubscription(req, res) {
-    const subscriptionRequest = req.body;
-    const susbscriptionId = createHash(JSON.stringify(subscriptionRequest));
-    subscriptions[susbscriptionId] = subscriptionRequest;
-    
-    res.status(201).json({ id: susbscriptionId, status: 'OK'});
+  const subscriptionRequest = req.body;
+  const susbscriptionId = createHash(JSON.stringify(subscriptionRequest));
+  subscriptions[susbscriptionId] = subscriptionRequest;
+
+  res.status(201).json({ id: susbscriptionId, status: 'OK' });
 }
 
 function sendPushNotification(req, res) {
-    const { repository } = req.body
-    
-    const subscriptionId = req.params.id;
-    const pushSubscription = subscriptions[subscriptionId];
-    console.log(repository);
-    
-    
-    webpush
-      .sendNotification(
-        pushSubscription,
-        JSON.stringify({
-          title: 'Woho github activity!',
-          text: repository.name,
-          url: repository.htlm_url,
-          image: '',
-        })
-      )
-      .catch(err => {
-        console.log(err);
-      });
+  const { repository } = req.body
+
+  const subscriptionId = req.params.id;
+  const pushSubscription = subscriptions[subscriptionId];
+  console.log(repository);
+
+  const options = JSON.stringify({
+    title: 'Woho github activity!',
+    text: repository.name,
+    url: repository.htlm_url,
+    image: '',
+  });
+
+  console.log('[options]', options);
   
-    res.status(202).json({});
-  }
+
+  webpush
+    .sendNotification(
+      pushSubscription,
+      options
+    )
+    .catch(err => {
+      console.log(err);
+    });
+
+  res.status(202).json({});
+}
 
 
 app.post('/', handlePushNotificationSubscription);
@@ -72,7 +76,7 @@ app.get('/:id', sendPushNotification);
 app.post('/:id', sendPushNotification)
 
 app.listen(port, () => {
-    if (host) {
-        console.info(`Server running at http://${host}:${port}/`);
-    }
+  if (host) {
+    console.info(`Server running at http://${host}:${port}/`);
+  }
 });
